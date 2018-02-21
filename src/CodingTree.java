cb04fad187e88f70e42ee3ac7ca990ab7225a388
Evigold <Evigold@users.noreply.github.com>
2018-02-21 07:57:57 -0800

    Updated coding tree to what we did on monday and almost finished main.
    Compression ratio not working. Not sure why

--- src/CodingTree.java
+++ src/CodingTree.java
@@ -1,81 +1,86 @@
 import java.util.*;
+
 import java.io.*;
 
 public class CodingTree {
 
-    //lala
-    private HuffmanNode huffmanTree;
+	private HuffmanNode huffmanTree;
 
-    public static final int CHAR_MAX = 256;  // max char value to be encoded
+	public static final int CHAR_MAX = 256;  // max char value to be encoded
+	public static final boolean DEBUG = false; // set to true to print ASCII 0s
 
-    public Map<Character, String> codes;
+	private static final int BYTE_SIZE = 8; // digits per byte
+	
+	public Map<Character, String> codes;
 
-    public String bits;
+	public String bits;
 
-    public CodingTree(String message) {
-        codes = new HashMap<Character, String>();
-        int[] counts = new int[CHAR_MAX];
-        for(int i = 0; i < message.length(); i++) {
-            counts[(int)message.charAt(i)]++;
+	public CodingTree(String message) throws IOException {
+		codes = new HashMap<Character, String>();
+		int[] counts = new int[CHAR_MAX];
+		for(int i = 0; i < message.length(); i++) {
+			counts[(int)message.charAt(i)]++;
+		} 
+		PriorityQueue<HuffmanNode> queue = new PriorityQueue<HuffmanNode>();
+		for(int i = 0; i < counts.length; i++) {
+			if(counts[i] != 0) {
+				queue.add(new HuffmanNode(counts[i], i));
+			}
+		}
+		queue.add(new HuffmanNode(1,256));
+		while(queue.size() > 1) {
+			HuffmanNode childOne = queue.poll();
+			HuffmanNode childTwo = queue.poll();
+			HuffmanNode temp = new HuffmanNode(childOne.getFrequency() + childTwo.getFrequency(), -1);
+			temp.setZero(childOne);
+			temp.setOne(childTwo);
+			queue.add(temp);
+		}
+		huffmanTree = queue.poll();
+		write();
+		encode(message);
+	}
+
+	public void write() throws IOException {
+		PrintStream output = new PrintStream(new File("codes.txt"));
+		write(huffmanTree, "");
+		for(Character key : this.codes.keySet()) {
+        		output.println(key);
+        		output.println(this.codes.get(key));
+
         }
-        PriorityQueue<HuffmanNode> queue = new PriorityQueue<HuffmanNode>();
-        for(int i = 0; i < counts.length; i++) {
-            if(counts[i] != 0) {
-                queue.add(new HuffmanNode(counts[i], i));
-            }
-        }
-        queue.add(new HuffmanNode(1,256));
-        while(queue.size() > 1) {
-            HuffmanNode childOne = queue.poll();
-            HuffmanNode childTwo = queue.poll();
-            HuffmanNode temp = new HuffmanNode(childOne.getFrequency() + childTwo.getFrequency(), -1);
-            temp.setZero(childOne);
-            temp.setOne(childTwo);
-            queue.add(temp);
-        }
-        huffmanTree = queue.poll();
-    }
+		output.close();
+		
+	}
 
-    public void write() {
-        write(huffmanTree, "");
-    }
+	// Helper method for the writing method.
+	// It takes a printStream, a HuffmanNode and a String as parameters.
+	private void write(HuffmanNode root, String HuffmanCode) {
+		if(root.getZero() != null) {
+			write(root.getZero(), (HuffmanCode + "0"));
+			write(root.getOne(), (HuffmanCode + "1"));
+		} else {
+			codes.put(root.getChar(), HuffmanCode);
+		}
+	}
 
-    // Helper method for the writing method.
-    // It takes a printStream, a HuffmanNode and a String as parameters.
-    private void write(HuffmanNode root, String HuffmanCode) {
-        if(root.getZero() != null) {
-            write(root.getZero(), (HuffmanCode + "0"));
-            write(root.getOne(), (HuffmanCode + "1"));
-        } else {
-            codes.put(root.getChar(), HuffmanCode);
-        }
-    }
-
-    // Constructor of the HuffmanTree for the encode class and decode method.
-    // Takes a scanner as a parameter.
-    // Uses a helper method build for recursion building of the tree.
-    public CodingTree(Scanner input) {
-        while(input.hasNextLine()) {
-            int n= Integer.parseInt(input.nextLine()); // ASCII code
-            String code = input.nextLine();  // HuffmanCode
-            huffmanTree = build(huffmanTree, n, code);
-        }
-    }
-
-    // Helper method for the decode constructor of the HuffmanTree.
-    // Takes a HuffmanNode, an int and a String as parameters.
-    // it returns a HuffmanNode.
-    private HuffmanNode build(HuffmanNode root, int ascii, String code) {
-        if(root == null) {
-            root = new HuffmanNode();
-        }
-        if(code.equals("")) {
-            return new HuffmanNode(0,ascii);
-        } else if('0' == code.charAt(0)) {
-            root.setZero(build(root.getZero(), ascii, code.substring(1)));
-        } else if('1' == code.charAt(0)) {
-            root.setOne(build(root.getOne(), ascii, code.substring(1)));
-        }
-        return root;
-    }
+	private void encode(String message) throws IOException {
+		PrintStream output = new PrintStream(new File("compressed.txt"));
+		int digits = 0;
+		int numDigits = 0;
+		for(char c : message.toCharArray()) {
+			String temp = codes.get(c);
+			for(int i = 0; i < temp.length(); i++) {
+				int bit = (temp.charAt(i) - '0');
+				digits += bit << numDigits;
+				numDigits++;
+				if (numDigits == BYTE_SIZE) {
+					output.write(digits);
+					digits = 0;
+					numDigits = 0;
+				}
+			}
+		}
+		output.close();
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,48 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+    	System.out.println("This program makes a Huffman code for a file.");
+        System.out.println();
 
+        Scanner console = new Scanner(System.in);
+        System.out.print("input file name? ");
+        String inFile = console.nextLine();
+        
+        long startTime = System.nanoTime();
+        
+        FileInputStream input = new FileInputStream(inFile);
+        Scanner line = new Scanner(new File(inFile));
+        StringBuilder fileString = new StringBuilder();
+        while(line.hasNextLine()) {
+        		fileString.append(line.nextLine());
+        		fileString.append('\n');
+        		
+        }
+        CodingTree t = new CodingTree(fileString.toString());
+        
+        long endTime = System.nanoTime(); 
+		long elapsedTime = (endTime - startTime ) / 1000000;
+
+        File file = new File(inFile);
+        long fileSize = file.length();
+        System.out.println("Uncompressed file size : " + fileSize + " bytes");
+
+        File cfile = new File("compressed.txt");
+        long cfileSize = cfile.length();
+        System.out.println("Compressed file size : " + cfileSize + " bytes");
+
+        //Not Working 
+        double ratio = (cfileSize/fileSize)*100;
+        System.out.println("Compression ratio: " + ratio + '%');
+        
+        System.out.println("Running time:" + elapsedTime + " milliseconds");
+
+        
 
     }
 }
